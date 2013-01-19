e208ffea5a94803ae03255ffae143d57bc90ab00
oklinsong <oklinsong@gmail.com>
2013-01-19 23:14:09 +0800

    add mongo

--- application/controllers/goods.php
+++ application/controllers/goods.php
@@ -12,6 +12,10 @@ class Goods extends MY_Controller{
         parent::__construct();
         $this->load->model("Goods_model");
     }
+
+    /**
+     * 发布商品
+     */
     function publish(){
         $userId = $this->input->get_post('userId');
         $title = $this->input->get_post('title');
@@ -19,22 +23,86 @@ class Goods extends MY_Controller{
         $price = $this->input->get_post('price');
         $lon  = $this->input->get_post('lon');
         $lat = $this->input->get_post('lat');
+
         // TODO 验证参数
-         $this->Goods_model->insertNewGoods();
+
+        $goods = array(
+            "userId" => $userId,
+            "title" => $title,
+            "desc" => $desc,
+            "price"=> $price,
+            "lon"=>$lon,
+            "lat"=>$lat
+        );
+        $rt = $this->Goods_model->insertNewGoods($goods);
+        if($rt == 1){
+            echo "{'code':20000,'desc':'商品发布成功'}";
+        }else{
+            echo "{'code':10012,'desc':'商品发布错误'}";
+        }
+
     }
 
+    /**
+     * 编辑商品
+     */
     function edit(){
         $this->Goods_model->updateGoods();
     }
 
+    /**
+     * 获取商品详细信息
+     */
     function detail(){
         $userId = $this->input->get_post('userId');
         $goodsId = $this->input->get_post('goodsId');
         $goodsInfo = $this->Goods_model->getGoods($userId,$goodsId);
         if($goodsId){
-            echo json_encode($goodsId);
+            echo json_encode($goodsInfo);
         }else{
             echo "";
+        }
+    }
+
+    /**
+     * 下架商品
+     */
+    function offline(){
+        $userId = $this->input->get_post('userId');
+        $goodsId = $this->input->get_post('goodsId');
+        echo $this->Goods_model->offlineGoods($goodsId,$userId);
+    }
+
+    /**
+     * 获取商品列表
+     * 包括根据地理位置获取和获取某人的商品列表
+     */
+    function getList(){
+        $access_token = $this->input->get_post('access_token');
+        $count = $this->input->get_post('count');
+        $page = $this->input->get_post('page');
+
+        $filter = $this->input->get_post('filter');
+        $keyword = $this->input->get_post('keyword');
+        $status = $this->input->get_post('status');
+
+        if($filter=="2"){
+            $userId = $keyword;
+            $goodList = $this->Goods_model->getAllGoodsByUser($userId,(int)$status,(int)$count,(int)$page);
+            foreach($goodList as $row){
+                echo json_encode($row);
+                echo "<br/>";
+            }
+        }elseif($filter=="1"){
+            $lon = $this->input->get_post('lon');
+            $lat = $this->input->get_post('lat');
+            $near = $this->Goods_model->getNearGoodsByLocal((int)$lon,(int)$lat,(int)$count,(int)$page,(int)$status);
+            while($near->hasNext()){
+                echo json_encode($near->getNext());
+                echo "<br/>";
+            }
+
+
         }
     }
 }
--- application/models/goods_model.php
+++ application/models/goods_model.php
@@ -10,41 +10,49 @@ class Goods_model extends CI_Model{
 
     private $table_name_prefix = "goods0";
 
+    const GOOD_COLLECTION = "goods";
     function __construct(){
         parent::__construct();
-        //$this->load->database();
+        $this->goodsCol = $this->mongodb->selectCollection(self::GOOD_COLLECTION);
     }
 
     /**
      * 发布商品
      * @return mixed
      */
-    function insertNewGoods(){
-        $userId = $this->input->get_post('userId');
-        $goodsId = "".$userId+time()+random(5);
+    function insertNewGoods($goods){
+        $userId = $goods['userId'];
+
+        $goodsId = "".$userId.time().rand(10000,99999);
+
         $status = 0;
-        $title = $this->input->get_post('title');
-        $desc  = $this->input->get_post('desc');
-        $price = $this->input->get_post('price');
-        $lon  = $this->input->get_post('lon');
-        $lat = $this->input->get_post('lat');
+
         $publish_time = date("Y/m/d H:i:s");
         $modify_time = $publish_time;
-        $data = array(
-            'goodsid'=>$goodsId,
-            'title'=>$title,
-            'desc' => $desc,
+
+        $goods['goodsid'] = $goodsId;
+        $goods['status'] = 0;
+        $goods['publishtime'] = $publish_time;
+        $goods['modifytime'] = $modify_time;
+
+        $mongoData = array(
+            'goodsid' => $goodsId,
+            'title' => $goods['title'],
+            'desc' => $goods['desc'],
             'userid' => $userId,
-            'price' => $price,
+            'price' => $goods['price'],
             'publishtime'=>$publish_time,
             'modifytime'=>$modify_time,
-            'lon'=>$lon,
-            'lat'=>$lat,
+            'gps' => array(
+                'lon'=> (double)$goods['lon'],
+                'lat'=> (double)$goods['lat']
+            ),
             'status'=>$status
         );
         $table_name = $this->getTableName($userId);
-
-        return $this->db->insert($table_name,$data);
+        $insertRs = $this->db->insert($table_name,$goods);
+        $this->goodsCol->save($mongoData);
+        return $insertRs;
     }
 
     /**
@@ -72,8 +80,7 @@ class Goods_model extends CI_Model{
      * @param $userId
      * @return mixed
      */
-    function getGoods($goodsId,$userId){
-
+    function getGoods($userId,$goodsId){
         $rs = $this->db->get_where($this->getTableName($userId),array("goodsid" => $goodsId));
         return $rs->row_array();
     }
@@ -85,9 +92,50 @@ class Goods_model extends CI_Model{
      */
     function offlineGoods($goodsId,$userId){
         $sql = "UPDATE ".$this->getTableName($userId)." set status=1 where goodsid='".$goodsId."'";
-        $this->db->query($sql);
+        $mongoQuery = array(
+          "goodsid"=>$goodsId
+        );
+        $targetGoods = $this->goodsCol->findOne($mongoQuery)->getNext();
+        $targetGoods["status"] = 1;
+        $this->goodsCol->save($targetGoods);
+        return $this->db->query($sql);
     }
 
+    /**
+     * 获取某人的全部商品列表
+     * @param $userId
+     * @return mixed
+     */
+    function getAllGoodsByUser($userId,$status,$pageSize,$pageNo){
+
+        $sql = "SELECT * from ".$this->getTableName($userId)." where userid=".$userId;
+        if($status != 2 ){
+            $sql = $sql." and status=".$status;
+        }
+        $sql = $sql." order by publishtime desc limit ".(($pageNo-1)*$pageSize).",". $pageSize;
+        $rs =  $this->db->query($sql)->result();
+        return $rs;
+    }
+
+    /**
+     * 获取附近商品信息
+     * @param $lon
+     * @param $lat
+     */
+    function getNearGoodsByLocal($lon,$lat,$pageSize,$pageNo,$status){
+        $query =  array(
+            'gps'=>array("\$near"=> array('lon'=>$lon,'lat'=>$lat))
+        );
+        if($status != 2){
+            $query['status'] = $status;
+        }
+       return  $this->goodsCol->find($query)->skip(($pageNo-1)*$pageSize)->limit($pageSize);
+    }
+    /**
+     * 根据用户id获取表名
+     * @param $userId
+     * @return string
+     */
     private function getTableName($userId){
         return $this->table_name_prefix.($userId % 10);
     }
