771abcc28345f787e0eb7dc01331a7e2c3956638
zj831007 <zj831007@163.com>
2013-02-02 23:09:47 +0800

    fix

--- application/controllers/img.php
+++ application/controllers/img.php
@@ -38,11 +38,7 @@ class Img extends MY_Controller{
             tkProcessError("20004");
         }
 
-        if(empty($_POST["img"])){
-            tkProcessError("20003");
-        }
-
-
+        
 
         $img_path = $this->Img_model->checkUploadPath($uid, $type, $ext);
         if(empty($img_path)){
